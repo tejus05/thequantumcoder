6321152c0018fbe1025e5c2fa1588fa55f5eabd1
tejus <thequantumcoder@gmail.com>
2024-04-07 21:29:11 +0530

    adjust margin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ const HeroSection: React.FC = () => {
 
 
   return (
-    <section className="flex flex-col items-center justify-center px-4 md:flex-row md:justify-between md:px-8 lg:px-16 md:mt-12 -mt-20 w-full h-screen">
+    <section className="flex flex-col items-center justify-center px-4 md:flex-row md:justify-between md:px-8 lg:px-16 md:-mt-12 -mt-20 w-full h-screen">
       <div
         className="flex flex-col items-center text-center md:items-start md:text-left md:w-1/2"
         onMouseOver={() => {
